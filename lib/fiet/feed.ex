ae9a10eadfec81ccd24e9312b371c98521ee9587
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-02-25 20:29:23 +0100

    Introduce Fiet.Feed as the unified format for Atom/RSS2.0

---
+++ lib/fiet/feed.ex
@@ -0,0 +1,116 @@
+defmodule Fiet.Feed do
+  defstruct [
+    :title,
+    :link,
+    :description,
+    :updated_at,
+    categories: [],
+    items: []
+  ]
+
+  alias Fiet.{Atom, RSS2, Item}
+
+  def new(%Atom.Feed{} = feed) do
+    %{
+      title: title,
+      link: link,
+      subtitle: subtitle,
+      updated: updated,
+      categories: categories,
+      entries: entries
+    } = feed
+
+    %__MODULE__{
+      title: text_construct(title),
+      link: extract_atom_link(link),
+      description: text_construct(subtitle),
+      updated_at: updated,
+      categories: map_categories(categories),
+      items: map_items(entries)
+    }
+  end
+
+  def new(%RSS2.Channel{} = feed) do
+    %{
+      title: title,
+      link: link,
+      description: description,
+      last_build_date: last_build_date,
+      categories: categories,
+      items: items
+    } = feed
+
+    %__MODULE__{
+      title: title,
+      link: link,
+      description: description,
+      updated_at: last_build_date,
+      categories: map_categories(categories),
+      items: map_items(items)
+    }
+  end
+
+  defp map_categories(categories, acc \\ [])
+
+  defp map_categories([], acc), do: Enum.reverse(acc)
+
+  defp map_categories([%Atom.Category{} = category | categories], acc) do
+    %{term: term} = category
+    map_categories(categories, [term | acc])
+  end
+
+  defp map_categories([%RSS2.Category{} = category | categories], acc) do
+    %{value: value} = category
+    map_categories(categories, [value | acc])
+  end
+
+  defp map_items(items, acc \\ [])
+
+  defp map_items([], acc), do: Enum.reverse(acc)
+
+  defp map_items([%Atom.Entry{} = entry | items], acc) do
+    %{
+      id: id,
+      title: title,
+      link: link,
+      summary: summary,
+      published: published
+    } = entry
+
+    item = %Item{
+      id: id,
+      title: text_construct(title),
+      description: text_construct(summary),
+      link: extract_atom_link(link),
+      published_at: published
+    }
+
+    map_items(items, [item | acc])
+  end
+
+  defp map_items([%RSS2.Item{} = item | items], acc) do
+    %{
+      guid: guid,
+      title: title,
+      link: link,
+      description: description,
+      pub_date: pub_date
+    } = item
+
+    item = %Item{
+      id: guid,
+      title: title,
+      description: description,
+      link: link,
+      published_at: pub_date
+    }
+
+    map_items(items, [item | acc])
+  end
+
+  defp text_construct({_type, content}), do: content
+  defp text_construct(nil), do: nil
+
+  defp extract_atom_link(%Atom.Link{href: href}), do: href
+  defp extract_atom_link(nil), do: nil
+end
---
+++ lib/fiet/item.ex
@@ -0,0 +1,9 @@
+defmodule Fiet.Item do
+  defstruct [
+    :id,
+    :title,
+    :description,
+    :published_at,
+    :link
+  ]
+end
